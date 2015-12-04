d4bef1077dd3c64a12df545622f10948c3e99c5c
tangtangwzc <tangtangwzc@gmail.com>
2015-12-04 16:18:12 -0500

    implemented feedToStitching and panoramaGenerator that uses memory as the working image space

--- select_and_merge.py
+++ select_and_merge.py
@@ -1,14 +1,25 @@
-from PanoramaConstruction_TwoSidesMergingMethod import ImageStiching
-
 import os
 import string
 import cv2
+from panoramaGenerator import ImageStitching
 
-def string_integer_splitter(sample):
+def filename2number(filename):
 	"""
 	example: "8.0.jpg" -> 8.0
 	"""
-	return int(string.split(sample, '.')[0])
+	return int(string.split(filename, '.')[0])
+
+def number2filename(number):
+	"""
+	example: 8 -> "8.0.jpg"
+	"""
+	# note: making it integer would make it more straightforward
+	return str(number) + ".0.jpg"
+
+def test_number2filename():
+	print number2filename(8) == "8.0.jpg" 
+	print number2filename(100) == "100.0.jpg" 
+# print test_number2filename()
 
 class SelectAndMerge(object):
 	"""docstring for ClassName"""
@@ -23,7 +34,7 @@ class SelectAndMerge(object):
 
 	def get_sorted_raw_int(self):
 		self.raw = os.listdir(self.raw_path)
-		raw_int = map(string_integer_splitter, self.raw)
+		raw_int = map(filename2number, self.raw)
 		self.sorted_raw_int = sorted(raw_int)
 		return self.sorted_raw_int
 
@@ -39,10 +50,27 @@ class SelectAndMerge(object):
 
 		for start in range(0, self.total - self.patchNumber, self.start_offset):
 			patch_select = self.patch_selection(start, self.patchNumber, self.diff)
-			print patch_select
 			self.indexList.append(patch_select)
 
-	def foo(self):
+	def feedToStitching(self):
+		"""get images from each of the batches and feed into image stiching algorithm
+
+		then save each panorama ... (maybe in this function)
+		"""
+		for batch in self.indexList:
+			print batch
+			image_fns = map(number2filename, [self.sorted_raw_int[i] for i in batch])
+			image_paths = [os.path.join(self.raw_path, fn) for fn in image_fns]
+			im_st = ImageStitching(image_paths)
+			print map(type, im_st.working_images)
+			panorama = im_st.run()# call panorama stiching algorithm
+			print type(panorama), panorama.shape
+			cv2.imshow('window', panorama)
+			cv2.waitKey(0)
+
+			break
+
+	def readImageBatch(self, ):
 		patch_counter = 0
 		patch_number = 0
 		for filename in self.sorted_raw_int:
@@ -55,8 +83,9 @@ class SelectAndMerge(object):
 			# 	cv2.imshow("test", img)
 	  #       	cv2.waitKey(5)
 	        
-
-SelectAndMerge().generatePatchBatches()
+sm = SelectAndMerge()
+sm.generatePatchBatches()
+sm.feedToStitching()
 	# # merge
 # folderName = "street_view_images/working"
 # imageName = "image"
